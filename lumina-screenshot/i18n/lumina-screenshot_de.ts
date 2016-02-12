79be1b83ff5c37b40071e14d44701ede031ba09d
Alex <satriani-vai@users.noreply.github.com>
2016-02-12 14:04:11 +0100

    Update lumina-screenshot_de.ts

--- lumina-screenshot/i18n/lumina-screenshot_de.ts
+++ lumina-screenshot/i18n/lumina-screenshot_de.ts
@@ -6,86 +6,86 @@
     <message>
         <location filename="../MainUI.ui" line="14"/>
         <source>Take Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Bildschirmfoto aufnehmen</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="33"/>
         <source>No Screenshot Yet</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Noch kein Bildschirmfoto</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="43"/>
         <source>New Screenshot Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Neue Einstellungen für Bildschirmfoto</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="55"/>
         <source> Seconds</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"> Sekunden</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="58"/>
         <source>Delay </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Verzögerung </translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="68"/>
         <source>Entire Screen</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Gesamten Bildschirm</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="75"/>
         <location filename="../MainUI.cpp" line="63"/>
         <source>Select Window</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Fenster wählen</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="100"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Werkzeugleiste</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="129"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Speichern</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="132"/>
         <location filename="../MainUI.ui" line="135"/>
         <location filename="../MainUI.cpp" line="38"/>
         <source>Save Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Bildschirmfoto speichern</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="138"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">STRG+S</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="143"/>
         <location filename="../MainUI.ui" line="146"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Schließen</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="151"/>
         <source>Snap</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Snap</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="154"/>
         <source>Take new snapshot</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Neues Bildschirmfoto aufnehmen</translation>
     </message>
     <message>
         <location filename="../MainUI.cpp" line="38"/>
         <source>PNG Files (*.png);;AllFiles (*)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">PNG Dateien (*.png);;AlleDateien (*)</translation>
     </message>
     <message>
         <location filename="../MainUI.cpp" line="63"/>
         <source>Window:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Fenster:</translation>
     </message>
 </context>
 </TS>
